b7b5f7948a507b386acd67b382a18865e068901a
John R. James <john.james@usma.edu>
2011-04-26 15:03:27 -0400

    new file:   .gitignore         new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README 	new file:   Rakefile 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/welcome_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/helpers/welcome_helper.rb 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/welcome/index.html.erb 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/tasks/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/images/rails.png 	new file:   public/javascripts/application.js 	new file:   public/javascripts/controls.js 	new file:   public/javascripts/dragdrop.js 	new file:   public/javascripts/effects.js 	new file:   public/javascripts/prototype.js 	new file:   public/javascripts/rails.js 	new file:   public/robots.txt 	new file:   public/stylesheets/.gitkeep 	new file:   script/rails 	new file:   test/functional/welcome_controller_test.rb 	new file:   test/performance/browsing_test.rb 	new file:   test/test_helper.rb 	new file:   test/unit/helpers/welcome_helper_test.rb 	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '4033c0aa74eeb8606588ab889e67e018646e05e51bd240330f7b916575f9b0106492d46b5603d077d823d8b6d884006d073c6e55db352f397e8477df4a3bbed8'
